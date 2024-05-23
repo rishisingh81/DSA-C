6a76f97d3baeb3029cb5a297753e21c8243ada6e
Rishi Singh <167462373+rishisingh81@users.noreply.github.com>
2024-05-24 01:09:30 +0530

    Create b1.c

---
+++ Bubble/b1.c
@@ -0,0 +1,38 @@
+//Given an Integer N and a list arr. Sort the array using bubble sort algorithm
+#include <stdio.h>
+
+// Function to perform bubble sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements if they are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements you want to enter in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("The sorted array is:");
+    for (int i = 0; i < n; i++) {
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
